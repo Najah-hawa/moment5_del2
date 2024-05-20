dc951228928baba2cfb429ddacb57235bf4d20ef
Najah Hawa <najah.hawa@gmail.comm>
2024-05-21 01:27:02 +0300

    moment5_del2

---
+++ README.md
@@ -0,0 +1,17 @@
+# Hej!
+## Kort beskrivning av moment 5 del 2 i kursen Webbutveckling III
+momentet går ut på att skapa en webbapplikation som konsumerar webbtjänsten ssom gjorts i momentet del 1. 
+
+### andra del av moment 5-del1
+
+I det andra delen skapar jag en Webbplats för att läsa ut befintliga kurser ssom finns lägrade i databasen genom webbtjänst. dessutom finns det möjlighet för att lägga till nya via ett formulär. samt går det att radera kurser som man vill om man klickar på dem. 
+#### krav för denna del i moment är:
+1.  Webbplatsen ska använda Fetch API för att hämta (GET) och skicka data (POST) från och till webbtjänsten. jag använder också method (delete) för att radera en kurs. 
+2. Lösningen ska skapas med enbart HTML, CSS och JavaScript (ej PHP). SASS ska användas för din CSS. 
+#### arbetsgång:
+1. jag skapade en html sida med en formulär för att kunna läsa ut data till det och kunna skicka data. 
+2. sedan började jag med att skapa funktioner för att använda webbtjänst och hantera data genom att använda fetch api och kunsumera webbtjänst som vi tidigare skapat. 
+4. sist gjorde jag desin för css genom att använda sass och konvertera det med gulp till vanlig css. 
+
+## URl
+min url för min webbsida är: 
---
+++ src/js/main.js
@@ -0,0 +1,112 @@
+"use strict";
+
+window.onload = init;
+//let url = "http://localhost/php-api/cources/read.php";
+url = "http://studenter.miun.se/~naha2204/dt173g/php-api/cources/read.php";
+
+const Kurskodinput =document.getElementById("Kurskod");
+const Kursnamninput =document.getElementById("Kursnamn");
+const Progressioninput =document.getElementById("Progression");
+const Kursplaninput =document.getElementById("Kursplan");
+const submitbtn =document.getElementById("submit");
+const button =document.getElementById("button");
+submitbtn.addEventListener("click", createcource);
+function init(){
+getCourses();
+}
+
+//funktion för att läsa kurser lista på DOM
+function getCourses(){
+fetch(url)
+.then(response => {
+    if(response.status != 200){ //inte lyckat anrop bara return
+        return
+    }
+    return response.json()
+.then(data => writecources(data))
+.catch(err => console.log(err))
+})
+}
+
+//skriva kurser som li element
+function writecources(cources){
+
+  const ulEl = document.getElementById("courslist");
+  ulEl.innerHTML ="";
+  cources.forEach(cource => {
+     ulEl.innerHTML += `<li class="cource" id="${cource.id}" >Kurskod: ${cource.Kurskod}, Kursnamn:  ${cource.Kursnamn},
+     <br> Progression: ${cource.Progression}, Kursplan: ${cource.Kursplan}</li>`
+  });
+let liEL= document.getElementsByClassName("cource");
+for(let i=0; i<liEL.length; i++){
+  liEL[i].addEventListener("click",deletecoure);
+}
+
+}
+
+
+//radera en kurs med id 
+function deletecoure(event){
+  //spara id i variabel
+  let id = event.target.id;
+// fetch och skicka method delete
+  fetch(url + "?id=" + id, {
+    "method" : "DELETE"
+  })
+  .then(response => response.json()) 
+  .then(data => getCourses() )   //läsa kurserlista på nytt efter att radera en kurs
+  .catch(err => console.log(err))
+  }
+
+  
+  
+    
+  
+
+function createcource(event){
+event.preventDefault();
+//läsa in värden från input
+let Kurskodin = Kurskodinput.value;
+let Kursnamnin = Kursnamninput.value;
+let Progressionin = Progressioninput.value;
+let Kursplanin = Kursplaninput.value;
+
+let jsonStr = JSON.stringify({
+  Kurskod : Kurskodin,
+  Kursnamn :Kursnamnin,
+  Progression : Progressionin,
+  Kursplan :Kursplanin
+})
+fetch(url , {
+  method : "POST",
+  headers: {
+    "content-type" : "application/json"
+  }, 
+  body: jsonStr
+
+})
+.then(response => response.json()) 
+.then(data => clearform() )   //läsa kurserlista på nytt efter att radera en kurs
+.catch(err => console.log(err))
+}
+
+//rensa form
+function clearform(){
+  Kurskodinput.value="";
+  Kursnamninput.value="";
+  Progressioninput.value="";
+  Kursplaninput.value="";
+  getCourses();
+}
+  
+
+
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
